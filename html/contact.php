ba703e764d3137efe75cd5ab2a68979389b0ac56
Kassie Wang <kassielw@users.noreply.github.com>
2020-06-08 22:02:23 -0400

    Contact form

---
+++ html/contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="klw242@cornell.edu";
+    $subject="Personal Website Contact Form";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
